4be506c4f440f4a5641d5fe0260f18dcaf563439
rabinabaga <rabinabaga@gmail.com>
2024-03-17 17:27:42 +0545

    carousel app, in 40 minutes

---
+++ carousel/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import './App.css'
+
+function App() {
+  const images = [
+    "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    activeIndex > 0
+      ? setActiveIndex(activeIndex - 1)
+      : setActiveIndex(images.length - 1);
+  };
+
+  const handleNextClick = () => {
+    activeIndex < images.length - 1
+      ? setActiveIndex(activeIndex + 1)
+      : setActiveIndex(0);
+  };
+  return (
+    <>
+      <p className="heading">Project 1: Carousel</p>
+      <section className="carousel">
+        <button className="changeImageButton" onClick={handlePrevClick}>prev</button>
+        <img src={images[activeIndex]} alt="" />
+        <button className="changeImageButton"  onClick={handleNextClick}>next</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
--- my-notes-app/src/main.jsx
+++ my-notes-app/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import FeedbackForm from './HotelFeedbackForm.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/hotel",
+    element: <FeedbackForm/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 )
